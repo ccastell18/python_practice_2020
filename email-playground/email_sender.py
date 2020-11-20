6369215f01b01a5c8ea47776d9c28ab709de558b
Christopher Castellano <c.castellano18@gmail.com>
2020-11-19 23:20:29 -0600

    email send successfully

--- email-playground/email_sender.py
+++ email-playground/email_sender.py
@@ -1,21 +1,16 @@
 import smtplib
 from email.message import EmailMessage
 
-
-# email object
 email = EmailMessage()
-email["from"] = "Christopher Castellano"
+email["from"] = "Chris"
 email["to"] = "ccastell_1@hotmail.com"
-email["subject"] = "You won 1,000,000 dollars!"
+email["subject"] = "You won a million dollars!"
 
-email.set_content("I am a Python Master")
+email.set_content("I am a Python master")
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
-    # starts server
     smtp.ehlo()
-    # encryption
     smtp.starttls()
-    # logs into server
-    smtp.login("christopher.castellano1980@gmail.com", "Jimmyfarbez1")
+    smtp.login("c.castellano1980@gmail.com", "Jimmyfarbez1")
     smtp.send_message(email)
-    print("all good boss!")
+    print("all good boss")
