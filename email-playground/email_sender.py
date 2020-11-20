69cf3fb6fffc4de37a6457a38b306718e871530c
Christopher Castellano <c.castellano18@gmail.com>
2020-11-19 23:28:30 -0600

    email sent sucessfully using HTML

--- email-playground/email_sender.py
+++ email-playground/email_sender.py
@@ -1,12 +1,16 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path("index.html").read_text())
 email = EmailMessage()
 email["from"] = "Chris"
 email["to"] = "ccastell_1@hotmail.com"
 email["subject"] = "You won a million dollars!"
 
-email.set_content("I am a Python master")
+# email.set_content("I am a Python master")
+email.set_content(html.substitute({"name": "Tim"}), "html")
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     smtp.ehlo()
